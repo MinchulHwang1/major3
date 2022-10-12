b863cc186683831df1f57b587795ae3168b08d97
Minchul Hwang <hmc912@gmail.com>
2022-10-11 21:22:17 -0400

    3rd revision - get name if there is blank line - fail

--- m3.cpp
+++ m3.cpp
@@ -20,13 +20,38 @@ int main() {
 	int x = 0;
 	int y = 0;
 	double z = 0;
-	//string b = 0;
 
 
+	for (i = 0; i < a; i++) {
+
+
+		getline(cin, attName[i]); //이름에 배열 인자를 넣었다.
+		if (attName[i] == "") {   //If there is no name in the attName array
+			error;
+		}
+		else {
+			getline(cin, checkInDay[i]);
+			getline(cin, checkOutDay[i]);
+			x = dayToIndex(checkInDay[i]);
+			y = dayToIndex(checkOutDay[i]);
+			z = calculateCostOfRoom(roomRate, x, y);
+
+			cout << attName[i] << " attendancd name " << endl;
+			cout << "************** " << x << endl;
+			cout << "--/*-/*-/*-/* " << y << endl;
+			cout << "calculated price : " << z << endl;
+		}
+	}
+
+
+
+
+	//under code is safe. not error without attName[i]
+	/*
 	//이름 넣으면 에러가 뜨는것을 진행
 	for (i = 0; i < a; i++) {
 		
-		//cout << attName[i] << endl;
+		
 		getline(cin, attName[i]);
 		getline(cin, checkInDay[i]);
 		getline(cin, checkOutDay[i]);
@@ -34,15 +59,11 @@ int main() {
 		y = dayToIndex(checkOutDay[i]);
 		z = calculateCostOfRoom(roomRate, x, y);
 
-		cout << attName[i] << "   " << endl;
-
-		cout << "--------------" << x << endl;
-		//y = dayToIndex(checkOutDay[i]);
-		cout << "----++++++++++++---" << y << endl;
-		//z = calculateCostOfRoom(roomRate, x, y); 
-
-		cout << z << endl;
-	}
+		cout << attName[i] << " attendancd name " << endl;
+		cout << "************** " << x << endl;
+		cout << "--/*-/*-/*-/* " << y << endl;
+		cout << "calculated price : " << z << endl;
+	}*/
 		return 0;
 	
 }
